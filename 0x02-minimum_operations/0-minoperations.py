40d5da040a88fd42585f27caf7b920bae71e7803
Odarell35 <leratomawasha0@gmail.com>
2024-03-23 07:58:44 +0200

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module"""
+
+
+def minOperations(n):
+    """ calculates the fewest number of operations needed
+        to result in exactly n H
+        characters in the file."""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    current_h = 1
+    clipboard = current_h
+    
+    while current_h < n:
+        if n % current_h == 0:
+            clipboard = current_h
+            operations += 1
+        
+        current_h += clipboard
+        operations += 1
+    return operations
