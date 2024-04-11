f041b0495e124a4b045ee627ea6ee453ec838697
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-11 14:34:57 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,57 +2,54 @@
 """Module"""
 import sys
 
-def is_safe(board, row, col, n):
+def is_safe(board, row, col):
     """Check if there is a queen in the same column"""
     for i in range(row):
-        if board[i][col] == 'Q':
+        if board[i] == col:
             return False
-    
-    # Check upper left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
+        # Check diagonals
+        if abs(board[i] - col) == abs(i - row):
             return False
-    
-    # Check upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 'Q':
-            return False
-    
     return True
 
-def solve_nqueens(board, row, n):
-    """solve """
-    if row == n:
-        for r in board:
-            print(''.join(r))
-        print()
-        return
-    
-    for col in range(n):
-        if is_safe(board, row, col, n):
-            board[row][col] = 'Q'
-            solve_nqueens(board, row + 1, n)
-            board[row][col] = '.'
-
-def nqueens(N):
+def solve_nqueens_util(board, row, n, solutions):
     """method"""
-    try:
-        N = int(N)
-    except ValueError:
+    if row == n:
+        solutions.append(board[:])
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens_util(board, row + 1, n, solutions)
+            board[row] = -1
+
+def solve_nqueens(n):
+    """method"""
+    if not n.isdigit():
+        print("N must be a number")
         sys.exit(1)
-    
-    if N < 4:
+
+    n = int(n)
+    if n < 4:
+        print("N must be at least 4")
         sys.exit(1)
-    
-    board = [['.' for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0, N)
+
+    board = [-1] * n
+    solutions = []
+    solve_nqueens_util(board, 0, n, solutions)
+
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(n)])
 
 if __name__ == "__main__":
-    """main"""
+    """run main"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
+    solve_nqueens(sys.argv[1])
+
     nqueens(sys.argv[1])
 
 
