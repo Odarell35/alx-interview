1cc51df76eece4651eed5dd849dfe24c05f047c9
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-17 09:15:39 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,36 @@
 #!/usr/bin/node
-""" Star Wars Characters"""
-import sys
-import requests
+""" Prints all characters of a Star Wars movie """
 
-def get_movie_characters(movie_id):
-    """prints all characters of a Star Wars movie"""
+const axios = require('axios');
 
-    url = f"https://swapi.dev/api/films/{movie_id}/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        characters_urls = data['characters']
-        for character_url in characters_urls:
-            character_response = requests.get(character_url)
-            if character_response.status_code == 200:
-                character_data = character_response.json()
-                print(character_data['name'])
-            else:
-                print(f"Failed to fetch character data for URL: {character_url}")
-    else:
-        print(f"Failed to fetch movie data for ID: {movie_id}")
+function getMovieCharacters(movieId) {
+    const url = `https://swapi.dev/api/films/${movieId}/`;
+    
+    axios.get(url)
+        .then(response => {
+            const charactersUrls = response.data.characters;
+            
+            charactersUrls.forEach(characterUrl => {
+                axios.get(characterUrl)
+                    .then(characterResponse => {
+                        console.log(characterResponse.data.name);
+                    })
+                    .catch(error => {
+                        console.error(`Failed to fetch character data for URL: ${characterUrl}`);
+                    });
+            });
+        })
+        .catch(error => {
+            console.error(`Failed to fetch movie data for ID: ${movieId}`);
+        });
+}
+
+if (process.argv.length !== 3) {
+    console.log("Usage: node script.js <movie_id>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+getMovieCharacters(movieId);
+
 
